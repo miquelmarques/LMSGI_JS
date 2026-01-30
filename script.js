c3ecc0ea4065c727bebd0a89de60973397c3f4ed
Miquel Marquès <miquelmb11@gmail.com>
2026-01-30 10:01:40 +0100

    Update script.js S'ha modificat el script per tal de poder aconseguir no tenir resultats en negatiu i problemes de missatges

--- script.js
+++ script.js
@@ -2,25 +2,26 @@ let numeroSecret;
 let intentsMaxims;
 let intentsFet = 0;
 let numeroIntents;
+let IntentsRestants = 1;
 
 function guardarIntents() {
     let numeroIntents = document.getElementById("nintents").value;
-    if (numeroIntents > 0) {
+    if (numeroIntents > 0 && numeroIntents < 20) {
         intentsMaxims = numeroIntents;
         numeroSecret = Math.floor(Math.random() * 100) + 1;
         document.getElementById("IntentsRestants").innerHTML = "Intents restants: " + intentsMaxims;
         intentsFet = 0;
     }
     else {
-        alert("Introdueix un numero de 1 a 20");
+        document.getElementById("missatge").innerHTML = "Introdueix un numero de 1 a 20";
     }
 
 }
 function jugar() {
     let INnum = document.getElementById("numeroIntroduit").value;
-    if (INnum > 0 && INnum <= 100) {
+    if (INnum > 0 && INnum <= 100 && IntentsRestants > 0) {
         intentsFet++;
-        let IntentsRestants = intentsMaxims - intentsFet;
+        IntentsRestants = intentsMaxims - intentsFet;
         document.getElementById("IntentsRestants").innerHTML = "Intents restants: " + IntentsRestants;
         if (INnum == numeroSecret) {
             document.getElementById("missatge").innerHTML = "Correcte, Has endevinat el número";
@@ -39,10 +40,11 @@ function jugar() {
 
             document.getElementById("missatge").innerHTML = "El número secret és més PETIT";
         }
-        else {
+        else if (INnum > 100) {
             document.getElementById("missatge").innerHTML = "El número secret és més GRAN";
 
 
-        }
+        }  
     }
 }
+
